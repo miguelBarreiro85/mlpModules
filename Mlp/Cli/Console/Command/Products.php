56481966b8bc127334e182435d4e870d8d841c2c
Miguel <miguelbarreiro85@gmail.com>
2018-09-29 16:04:28 +0100

    tax2

--- Mlp/Cli/Console/Command/Products.php
+++ Mlp/Cli/Console/Command/Products.php
@@ -310,7 +310,7 @@ class Products extends Command
                     $product->setCustomAttribute('ts_dimensions_length',(int)$data[21]/10);
                     $product->setCustomAttribute('ts_dimensions_width',(int)$data[22]/10);
                     $product->setCustomAttribute('ts_dimensions_height',(int)$data[23]/10);
-                    $product->setCustomAttribute('tax_class_id',1);
+                    $product->setCustomAttribute('tax_class_id',2);
                     $product->setWeight($data[19]);
                     $product->setLength(50);
                     $product->setCustomAttribute('width',(int)$data[22]);
